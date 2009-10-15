adba55a0182d0fbaf68adbd7a9dd9c8950d14321
Jordan Bracco <jordan@bracco.name>
2009-10-15 23:12:30 +0200

    Update search URLs

--- lib/allocine.rb
+++ lib/allocine.rb
@@ -5,7 +5,7 @@ require 'iconv'
 require 'allocine/allocine'
 require 'allocine/movie'
 require 'allocine/show'
-MOVIE_SEARCH_URL = "http://www.allocine.fr/recherche/?motcle=%s&rub=1"
+MOVIE_SEARCH_URL = "http://www.allocine.fr/recherche/1/?q=%s"
 MOVIE_DETAIL_URL = "http://www.allocine.fr/film/fichefilm_gen_cfilm=%s.html"
-SHOW_SEARCH_URL = "http://www.allocine.fr/recherche/?motcle=%s&rub=6"
+SHOW_SEARCH_URL = "http://www.allocine.fr/recherche/6/?q=%s"
 SHOW_DETAIL_URL = "http://www.allocine.fr/series/ficheserie_gen_cserie=%s.html"
