09f6794bb972956be0bfa25bada3aa92ff269e1c
Jordan Bracco <jordan@bracco.name>
2009-11-19 01:16:54 +0100

    Moved Allocine module (lib/allocine/allocine.rb) into lib/allocine.rb

--- lib/allocine.rb
+++ lib/allocine.rb
@@ -2,7 +2,7 @@ $:.unshift File.dirname(__FILE__)
 
 require 'activesupport'
 
-%w{allocine/web allocine/allocine allocine/movie allocine/show}.each do |lib|
+%w{allocine/web allocine/movie allocine/show}.each do |lib|
   require lib
 end
 
@@ -17,7 +17,28 @@ rescue LoadError
   Allocine::Web.send(:include, Allocine::WebOpenUri)
 end
 
-MOVIE_SEARCH_URL = "http://www.allocine.fr/recherche/1/?q=%s"
-MOVIE_DETAIL_URL = "http://www.allocine.fr/film/fichefilm_gen_cfilm=%s.html"
-SHOW_SEARCH_URL = "http://www.allocine.fr/recherche/6/?q=%s"
-SHOW_DETAIL_URL = "http://www.allocine.fr/series/ficheserie_gen_cserie=%s.html"
+module Allocine
+  MOVIE_SEARCH_URL = "http://www.allocine.fr/recherche/1/?q=%s"
+  MOVIE_DETAIL_URL = "http://www.allocine.fr/film/fichefilm_gen_cfilm=%s.html"
+  SHOW_SEARCH_URL = "http://www.allocine.fr/recherche/6/?q=%s"
+  SHOW_DETAIL_URL = "http://www.allocine.fr/series/ficheserie_gen_cserie=%s.html"
+  
+  # Make a search on movies
+  def self.find_movie(search)
+    Allocine::Movie.find(search)
+  end
+  
+  # Make a search on shows
+  def self.find_show(search)
+    Allocine::Show.find(search)
+  end
+  
+  # Returns the first result
+  def self.lucky_movie(search)
+    Allocine::Movie.lucky_find(search)
+  end
+  
+  def self.lucky_show(search)
+    Allocine::Show.lucky_find(search)
+  end
+end
