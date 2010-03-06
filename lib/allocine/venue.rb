b57f503bb976040960f8402c7899be6a8d37cbfa
Loïc CHOLLIER <loic@chollier.com>
2010-03-06 14:42:18 +0100

    On a maintenant les films en fonction de chaque salle

--- lib/allocine/venue.rb
+++ lib/allocine/venue.rb
@@ -1,5 +1,8 @@
 module Allocine
   class Venue
+    attr_accessor :id, :name, :adress, :movies
+    
+    
     def self.find(search)
       search = ActiveSupport::Multibyte.proxy_class.new(search.to_s).mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n,'').downcase.to_s.
         downcase.
@@ -27,5 +30,32 @@ module Allocine
       results = find(search)
       new(results.first)
     end
+    
+    def initialize(id, debug=false)
+      self.id = id
+      regexps = {
+        :name => '<div class="titlebar"><h1>(.*?)<\/h1>',
+        :adress => '<span class="bold">Adresse : <\/span>(.*?)<br \/>'
+      }
+      data = Allocine::Web.new(VENUE_DETAIL_URL % id).data.gsub(/\r|\n|\t/,"")
+      regexps.each do |reg|
+        print "#{reg[0]}: " if debug
+        r = data.scan Regexp.new(reg[1], Regexp::MULTILINE)
+        r = r.first.to_s.strip
+        r.gsub!(/<.*?>/, '')
+        r.strip!
+        self.instance_variable_set("@#{reg[0]}", r)
+        print "#{r}\n" if debug
+      end
+      self.movies = Array.new
+      
+      while data =~ /<a href='#movie(\d+)'>/mi
+        self.movies << Allocine::Movie.new($1)
+        data.gsub!("<a href='#movie#{$1}'>", "")
+      end
+      
+      
+    end
+    
   end
 end
